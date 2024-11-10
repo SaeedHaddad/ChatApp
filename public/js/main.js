14d75141043219a56b7b3f6ea37d98d3f36c122b
Saeed Haddad <haddadsaeed6@gmail.com>
2024-11-10 12:58:12 +0300

    pre-redis

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,81 @@
 //client-side javascript
-const chatForm = document.getElementById('chat-form');//to access the form element in chat.html
-const chatMessages = document.querySelector('.chat-messages')
+const chatForm = document.getElementById("chat-form"); //to access the form element in chat.html
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 //Get username and room from URL
-const {username, room} = Qs.parse(location.search,{
-    ignoreQueryPrefix: true
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
 });
 
-
-const socket = io();//this is accessible through the script added in chat.html
+const socket = io(); //this is accessible through the script added in chat.html
 
 //Join Chatroom
-socket.emit('joinRoom', {username, room})
+socket.emit("joinRoom", { username, room });
 
-
+//Get room and users
+socket.on('roomUsers', ({room, users})=>{
+    outputRoomName(room);
+    outputUsers(users);
+})
 
 //Message from server
-socket.on('message', message =>{
-    console.log(message);
-    outputMessage(message);
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
 
-    //Scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-})
-
+  //Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
 //Message submit
-chatForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    //Get message text
-    const msg = e.target.elements.msg.value; //when we submit the form this gets the message from the text input and then log it on the client side.
-    //console.log(msg);
-    //Emit message to server
-    socket.emit('chatMessage', msg);
-    //Clear input
-    e.target.elements.msg.value = "";
-    e.target.elements.msg.focus();
-    
-})
+  //Get message text
+  const msg = e.target.elements.msg.value; //when we submit the form this gets the message from the text input and then log it on the client side.
+  //console.log(msg);
+  //Emit message to server
+  socket.emit("chatMessage", msg);
+  //Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
 
 //Output Message to DOM
-function outputMessage(message){
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML=`<p class = "meta">${message.username} <span>${message.time}</span></p>
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class = "meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
     ${message.text}
     </p>`;
-    document.querySelector('.chat-messages').appendChild(div);
+  document.querySelector(".chat-messages").appendChild(div);
 }
+
+//Add room name to DOM
+function outputRoomName(room){
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = '';
+    users.forEach((user) => {
+      const li = document.createElement('li');
+      li.innerText = user.username;
+      userList.appendChild(li);
+    });
+  }
+
+  // Listen for the "Leave Room" button click
+const leaveBtn = document.getElementById('leave-btn');
+
+leaveBtn.addEventListener('click', () => {
+  // Emit the 'leaveRoom' event to the server
+  socket.emit('leaveRoom');
+
+  // Optionally, redirect to the homepage or room list
+  window.location.href = 'index.html';  // Adjust the redirect as needed
+});
--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require("express"); //includes the express module so we can use 
 const http = require("http");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-const { userJoin, getCurrentUser } = require("./utils/users");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express(); //creates an instance of Express by calling express() and gives access to all its methods like (app.get, app.post, app.listen)
 const server = http.createServer(app);
@@ -18,28 +23,66 @@ const botName = "ChatCord Bot";
 io.on("connection", (socket) => {
   socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
-  
+
     socket.join(user.room);
     //Welcome current user
     socket.emit("message", formatMessage(botName, "Welcome to ChatApp!")); //socket.emit => sends message to the single user
 
     //Broadcast when a user connects
-  socket.broadcast
+    socket.broadcast
       .to(user.room)
       .emit(
         "message",
         formatMessage(botName, `${user.username} has joined the chat.`)
       ); //socket.broadcast.emit => sends a message to all the clients except the single client
-  });
+ 
+ //Send users and room info (sidebar)
+ io.to(user.room).emit('roomUsers', {
+  room: user.room,
+  users: getRoomUsers(user.room)
+ })
+ 
+ 
+    });
 
   //Listen for chatMessage
   socket.on("chatMessage", (msg) => {
-    io.emit("message", formatMessage("USER", msg));
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
+
+  // Handle user leaving the room manually
+socket.on('leaveRoom', () => {
+  const user = getCurrentUser(socket.id);  // Get the current user
+
+  if (user) {
+    // Remove the user from the room
+    userLeave(socket.id);
+
+    // Broadcast message that the user has left
+    io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat.`));
+
+    // Send updated users and room info (sidebar)
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  }
+});
 
   //Runs when client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, `${user.username} has disconnected.`)); //io.emit => sends a message to all clients in general
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat.`)); //io.emit => sends a message to all clients in general
+    
+     //Send users and room info (sidebar)
+ io.to(user.room).emit('roomUsers', {
+  room: user.room,
+  users: getRoomUsers(user.room)
+ })
+    }
   });
 });
 
