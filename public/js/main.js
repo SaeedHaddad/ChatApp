a53f68cde2e3eab294d86feb8dea0bb7387954f1
Saeed Haddad <haddadsaeed6@gmail.com>
2024-11-10 11:21:18 +0300

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+//client-side javascript
+const chatForm = document.getElementById('chat-form');//to access the form element in chat.html
+const chatMessages = document.querySelector('.chat-messages')
+
+//Get username and room from URL
+const {username, room} = Qs.parse(location.search,{
+    ignoreQueryPrefix: true
+});
+
+
+const socket = io();//this is accessible through the script added in chat.html
+
+//Join Chatroom
+socket.emit('joinRoom', {username, room})
+
+
+
+//Message from server
+socket.on('message', message =>{
+    console.log(message);
+    outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+
+//Message submit
+chatForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    //Get message text
+    const msg = e.target.elements.msg.value; //when we submit the form this gets the message from the text input and then log it on the client side.
+    //console.log(msg);
+    //Emit message to server
+    socket.emit('chatMessage', msg);
+    //Clear input
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
+    
+})
+
+//Output Message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML=`<p class = "meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+    ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+const path = require("path"); // includes the 'path' module to help with file and directory path manipulations
+const express = require("express"); //includes the express module so we can use it in the app.
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
+
+const app = express(); //creates an instance of Express by calling express() and gives access to all its methods like (app.get, app.post, app.listen)
+const server = http.createServer(app);
+const io = socketio(server);
+
+//Set static folder
+//we want the public folder set as the static folder so we can access the HTML files to display our frontend, so we write the following express code while including the path module which is a node js core module
+app.use(express.static(path.join(__dirname, "public")));
+const botName = "ChatCord Bot";
+
+//Run when a client connects
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+  
+    socket.join(user.room);
+    
+    //Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatApp!")); //socket.emit => sends message to the single user
+
+    //Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat.`)
+      ); //socket.broadcast.emit => sends a message to all the clients except the single client
+  });
+
+  //Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", formatMessage("USER", msg));
+  });
+
+  //Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, `${user.username} has disconnected.`)); //io.emit => sends a message to all clients in general
+  });
+});
+
+const PORT = process.env.PORT || 3000; // use the port from environment variables if available, otherwise default to 3000.
+
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)); // starts the server and makes it listen for incoming requests on the specified port.
